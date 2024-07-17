5c03d833c1ad16bf21602fa16edda06f35d526da
daniel-antonio-developer <danielantonio.dad52@gmail.com>
2024-07-17 11:10:41 +0100

    adding images to images folder

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import '@/assets/styles/global.css';
+import Navbar from "@/components/Navbar";
+
 
 export const metadata = {
     title: "Venda Casa Skill",
@@ -12,6 +14,7 @@ const MainLayout = ({children}) => {
   return (
     <html lang="en">
       <body>
+        <Navbar/>
         <div>{children} </div>
       </body>
     </html>
